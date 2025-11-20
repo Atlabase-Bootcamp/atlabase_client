243da305e2575691192db2a808704dbfa1dfa447
nestorgariglio <nestorgariglio33@gmail.com>
2025-11-19 23:04:28 -0300

    FRONT-2: Done

---
+++ src/features/auth/schemas/auth.schema.ts
@@ -0,0 +1,17 @@
+import z from "zod";
+
+export const loginSchema = z.object({
+  email: z.email().min(1, "El correo es requerido"),
+  password: z.string().min(8, "Mínimo 8 caracteres"),
+});
+
+export const registerSchema = z.object({
+  username: z.string().min(3, "El usuario debe tener al menos 3 caracteres"),
+  email: z.email().min(1, "El correo es requerido"),
+  first_name: z.string().min(1, "El nombre es requerido"),
+  last_name: z.string().min(1, "El apellido es requerido"),
+  password: z.string().min(8, "La contraseña debe tener al menos 8 caracteres"),
+});
+
+export type LoginInput = z.infer<typeof loginSchema>;
+export type RegisterInput = z.infer<typeof registerSchema>;
